3e6554887396fa27ecc994b99c451980cb5cb2d6
ozerngg <naze.ozer@robotistan.com>
2024-03-27 09:43:54 +0300

    sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ release = '2.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = 'sphinx_rtd_theme'[]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -23,4 +23,4 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'haiku'
+html_theme = 'sphinx_rtd_theme'
